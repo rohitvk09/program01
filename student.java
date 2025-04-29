c3577ba4a3578921fc89d9d46ffb62f110dccbf2
rohitvk09 <justlogin49@gmail.com>
2025-04-29 11:21:42 +0530

    “Saved”

---
+++ student.java
@@ -0,0 +1,13 @@
+package Student;
+
+public class student {
+    public static void main(String[] args) {
+        String name = "Rohit";
+        String branch = "BCA";
+        String id = "01fe23bca108";
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Branch: " + branch);
+        System.out.println("ID: " + id);
+    }
+}
